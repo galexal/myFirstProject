d02adb1816a604a154cd981c28dd9d220c4697ec
galexal <galexal@yandex.ru>
2024-02-05 22:27:51 +0400

    доделана домашка3

--- src/lesson_3/Homework3.java
+++ src/lesson_3/Homework3.java
@@ -2,10 +2,19 @@ package lesson_3;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Homework3 {
     public static void main(String[] args) {
+        geometryFormulas();
 
+        printNumbersFrom40to20();
+
+        printOddNumbersSum();
+
+        printLineNumber();
+
+        System.out.println(getGCD(15, 9));
     }
 
     private static void geometryFormulas() {
@@ -14,5 +23,40 @@ public class Homework3 {
         formulas.put("прямоугольник", "S=a*b; P=2*(a+b)");
         formulas.put("квадрат", "S=a*a; P=4*a");
         formulas.put("круг", "S=Pi*r*r; P=2*Pi*r");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
+            System.out.println("Введите название фигуры или \"выход\"");
+            input = scanner.nextLine();
+            System.out.println(formulas.getOrDefault(input, "Нет данных"));
+        } while (!input.equalsIgnoreCase("выход"));
+    }
+
+    private static void printNumbersFrom40to20() {
+        for (int i = 40; i >= 20; i--) {
+            System.out.println(i);
+        }
+    }
+
+    private static void printOddNumbersSum() {
+        int sum = 0;
+        for (int i = 21; i < 60; i += 2) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+    private static void printLineNumber() {
+        for (int i = 1; i <= 20 ; i++) {
+            System.out.println("Это строка номер " + i);
+        }
+    }
+
+    private static int getGCD(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return getGCD(b, a - b);
     }
 }
